823a7a65ef7fe3bda92a91a5c4fa9bfee738dcb9
Mujahid122418 <mujahid122418@gmail.com>
2020-02-18 16:50:05 +0500

    upload

---
+++ build/precache-manifest.27a6431b3833d2c01c5eca336e90cd11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e53b3da8fefbd3d3e2c2",
+    "url": "http://localhost/pwa-2/static/css/main.23a4bac5.chunk.css"
+  },
+  {
+    "revision": "e53b3da8fefbd3d3e2c2",
+    "url": "http://localhost/pwa-2/static/js/main.e53b3da8.chunk.js"
+  },
+  {
+    "revision": "bbff4c00a001a372a2f3",
+    "url": "http://localhost/pwa-2/static/js/runtime~main.bbff4c00.js"
+  },
+  {
+    "revision": "510986f1ca7e9fca1546",
+    "url": "http://localhost/pwa-2/static/css/2.a5a1c17c.chunk.css"
+  },
+  {
+    "revision": "510986f1ca7e9fca1546",
+    "url": "http://localhost/pwa-2/static/js/2.510986f1.chunk.js"
+  },
+  {
+    "revision": "b86e6dcf17c66df7e34e3c1c633ae25b",
+    "url": "http://localhost/pwa-2/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "http://localhost/pwa-2/precache-manifest.27a6431b3833d2c01c5eca336e90cd11.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("http://localhost/pwa-2/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
